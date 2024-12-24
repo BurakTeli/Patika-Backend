b13a99451888ec5e1636201541c84a250431cba9
BurakTeli <tellibrk216@gmail.com>
2024-12-25 01:29:09 +0300

    IfElseSwitchCase

---
+++ Week 1 /İfElseSwitchCase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Burak manava hoş geldiniz");
+        Console.WriteLine("Elma = 2 TL");
+        Console.WriteLine("Armut = 3 TL");
+        Console.WriteLine("Çilek = 4 Tl");
+        Console.WriteLine("Muz = 3 Tl");
+
+
+        Console.Write("Hangi meyveyi satın almak istersiniz? (Elma/Armut/Çilek/Muz/Diğer):");
+        string friut = Console.ReadLine();
+
+        int price;
+
+        switch (friut.ToLower())
+        {
+            case "elma":
+                price = 2;
+                break;
+            case "armut":
+                price = 3;
+                break;
+            case "çilek":
+                price = 4;
+                break;
+            case "muz":
+                price = 3;
+                break;
+            default:
+                price = 4;
+                break;
+        }
+
+        Console.WriteLine($"Seçtiğiniz meyvenin fiyatı: {price} TL");
+    }
+}
