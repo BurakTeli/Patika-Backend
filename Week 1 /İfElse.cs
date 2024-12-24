3463e930da839133e023fc305bcc1cb6cbb281bd
BurakTeli <tellibrk216@gmail.com>
2024-12-25 01:03:40 +0300

    İfElse

---
+++ Week 1 /İfElse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Lütfen bir sayı giriniz : ");
+        int number  = int.Parse(Console.ReadLine());
+
+        if (number > 10 )
+        {
+            Console.WriteLine("Girilen sayı 10' dan büyüktür");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı 10'dan küçüktür");
+        }
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Girilen sayı Çift sayıdır");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı tek sayıdır");
+        }
+    }
+
+}
