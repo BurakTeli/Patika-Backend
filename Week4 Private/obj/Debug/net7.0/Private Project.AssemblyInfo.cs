57d78a7af3a6db28cf5e1a3164372dfe420ed98e
BurakTeli <tellibrk216@gmail.com>
2025-01-05 02:22:00 +0300

    Private Project

--- Week4 Private/obj/Debug/net7.0/Private Project.AssemblyInfo.cs
+++ Week4 Private/obj/Debug/net7.0/Private Project.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("1_privateProje")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Private Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("1_privateProje")]
-[assembly: System.Reflection.AssemblyTitleAttribute("1_privateProje")]
+[assembly: System.Reflection.AssemblyProductAttribute("Private Project")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Private Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
