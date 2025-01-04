d526547b26a50dc315cffc118ebf5c872b088673
BurakTeli <tellibrk216@gmail.com>
2025-01-04 13:15:18 +0300

    Update Rastgele_Sayi_Bulma_Oyunu.cs

--- Week3/Rastgele_Sayi_Bulma_Oyunu.cs
+++ Week3/Rastgele_Sayi_Bulma_Oyunu.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int hedefSayi = random.Next(1, 101); // 1 ile 100 arasında rastgele bir sayı
+        int tahminHakki = 5; // Kullanıcının toplam hakkı
+        int tahmin;
+
+        Console.WriteLine("Rastgele Sayı Bulma Oyununa Hoş Geldiniz!");
+        Console.WriteLine("Bilgisayar 1 ile 100 arasında bir sayı tuttu. 5 hakkınız var!");
+
+        for (int hak = tahminHakki; hak > 0; hak--)
+        {
+            Console.Write($"\n{hak} hakkınız kaldı. Tahmininizi girin: ");
+            
+            // Kullanıcıdan tahmini al
+            if (int.TryParse(Console.ReadLine(), out tahmin))
+            {
+                if (tahmin == hedefSayi)
+                {
+                    Console.WriteLine("Tebrikler! Doğru tahmin ettiniz.");
+                    return; // Oyunu bitir
+                }
+                else if (tahmin < hedefSayi)
+                {
+                    Console.WriteLine("Yukarı!");
+                }
+                else
+                {
+                    Console.WriteLine("Aşağı!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Lütfen geçerli bir sayı girin!");
+                hak++; // Geçersiz girişte hakkı azaltmamak için
+            }
+        }
+
+        Console.WriteLine($"\nMaalesef hakkınız bitti. Doğru sayı: {hedefSayi}");
+    }
+}
