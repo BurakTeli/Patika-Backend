811e18063f0c2705ee48031f7f45c23a186ba7d5
Burak TELLİ <148264781+BurakTeli@users.noreply.github.com>
2025-01-17 15:05:22 +0300

    Update PatikaPlusGalaNight.cs

--- Week 5/PatikaPlusGalaNight.cs
+++ Week 5/PatikaPlusGalaNight.cs
@@ -1,12 +1,14 @@
-
+using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
-        
     {
+        // Hoşgeldiniz mesajı
         Console.WriteLine("Patika Plus Gala Gecesine Hoşgeldiniz.");
 
+        // Davetlilerin isimlerini içeren bir liste oluşturuluyor
         List<string> davetliler = new List<string>
         {
             "Bülent Ersoy",
@@ -18,8 +20,11 @@ class Program
             "Funda Arar",
             "Demet Akalin"
         };
+
+        // Başlık yazdırma
         Console.WriteLine("** Davetliler: ** ");
 
+        // foreach döngüsü ile davetlilerin isimlerini ekrana yazdırma
         foreach (string davetli in davetliler)
         {
             Console.WriteLine(davetli);
